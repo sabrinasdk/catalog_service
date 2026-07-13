80113a6de4f563357794879c2de713c849076fb8
sabrinasdk <sabrinakhazem@gmail.com>
2026-07-13 23:31:40 +0100

    Initial commit

---
+++ src/models/product.model.d.ts
@@ -0,0 +1,6 @@
+export declare class Product {
+    readonly name: string;
+    readonly price: number;
+    constructor(name: string, price: number);
+}
+//# sourceMappingURL=product.model.d.ts.map
